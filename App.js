514c6fc37848633028a609b53a01eeae40dd0b63
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-01-08 17:18:07 +0900

    2.3 preload를 훅으로 구현하자

--- App.js
+++ App.js
@@ -8,38 +8,17 @@ import { Ionicons } from '@expo/vector-icons'
 
 import AppLoading from 'expo-app-loading';
 
-import { Asset } from 'expo-asset';
+import { Asset, useAssets } from 'expo-asset';
 
-const loadFonts = (fonts) => (fonts.map(font => Font.loadAsync(font)))
-
-const loadImages = (images) => {
-  images.map(image => {
-    if (typeof image === "string") {
-      return Image.prefetch(image)
-    } else {
-      return Asset.loadAsync(image)
-    }
-  })
-}
 
 export default function App() {
-  const [ready, setReady] = useState(false);
-
-  const startLoading = async () => {
-    const fonts = loadFonts([Ionicons.font, Ionicons.font, Ionicons.font]);
-    const images = loadImages(require('./android.png'), "https://reactnative.dev/img/oss_logo.png")
-    await Promise.all([...fonts, ...images])
-  }
-
-  const onFinish = () => setReady(true)
-
-  if (!ready) {
+  const [assets] = useAssets([require('./android.png')])
+  const [loaded] = Font.useFonts(Ionicons.font)
+  //이미지와 폰트만 프리로드하고싶다면 위 두줄로 충분하다.
+  //하지만 각종 DB나 여러가지 파일도 preload하고 싶다면 이전의 방식을 따르자
+  if (!assets || !loaded) {
     return (
-      <AppLoading
-        startAsync={startLoading}
-        onFinish={onFinish}
-        onError={console.error}
-      />
+      <AppLoading />
     );
   }
   return <Text>We are done</Text>
