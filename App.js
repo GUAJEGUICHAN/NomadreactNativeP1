1ad2e08cdff4cda3158e777dcaa168821cae8696
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-01-08 15:50:39 +0900

    2.2 이미티 preload하기

--- App.js
+++ App.js
@@ -8,11 +8,16 @@ import { Ionicons } from '@expo/vector-icons'
 
 import AppLoading from 'expo-app-loading';
 
+import { Asset } from 'expo-asset';
+
 export default function App() {
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true)
   const startLoading = async () => {
     await Font.loadAsync(Ionicons.font)
+    await Asset.loadAsync(require('./android.png'));
+    // await Image.prefetch(주소);
+
   }
 
   if (!ready) {
