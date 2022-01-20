40f8ddf691894ff83f15c8187d5807607f485eac
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-01-20 21:22:48 +0900

    2.15 theme.jsx 만듬

---
+++ theme.jsx
@@ -0,0 +1,19 @@
+import {
+  BLACK_COLOR,
+  LIGHT_GRAY_COLOR,
+  ORANGE_COLOR,
+  WHITE_COLOR,
+} from './colors';
+
+export const darkTheme = {
+  bgColor: BLACK_COLOR,
+  textColor: WHITE_COLOR,
+  ActiveTintColor: ORANGE_COLOR,
+  InactiveTintColor: LIGHT_GRAY_COLOR,
+};
+export const lightTheme = {
+  bgColor: WHITE_COLOR,
+  textColor: BLACK_COLOR,
+  ActiveTintColor: ORANGE_COLOR,
+  InactiveTintColor: LIGHT_GRAY_COLOR,
+};
