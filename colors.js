36a6d544633ad96de97511074f01f57ea0d93260
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-01-20 16:36:57 +0900

    2.8 [구현] colors.js - 컬러 프리셋

--- colors.js
+++ colors.js
@@ -2,4 +2,4 @@ export const BLACK_COLOR = "#1e272e"
 export const GRAY_COLOR = '#485460'
 export const LIGHT_GRAY_COLOR = '#808e9b'
 export const WHITE_COLOR = '#d2dae2'
-
+export const ORANGE_COLOR = '#ffa801'
